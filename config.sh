15c3cafcb080128d441f6b78a3585bc503f792f6
David Frech <david@nimblemachines.com>
2010-03-28 15:35:42 -0700

    Removed count-weeks.sh from config.sh, crontab, and .gitignore
    We're going to try to do this in a way that actually works. Since my
    computer was often turned off during the Thursday afternoons when the
    count-weeks script would run (via cron), it never counted time properly
    and wouldn't do the 28 day update.
    
    Doing math on Unix epoch-based times should work much better. That's
    where I'm headed next.

--- config.sh
+++ config.sh
@@ -2,8 +2,8 @@
 
 here=$(pwd)
 
-for f in crontab update-ip.sh count-weeks.sh; do
+for f in crontab update-ip.sh ; do
     sed -e "s#\$scriptdir#$here#g" < $f.in > $f
 done
-chmod 755 update-ip.sh count-weeks.sh
+chmod 755 update-ip.sh
 
